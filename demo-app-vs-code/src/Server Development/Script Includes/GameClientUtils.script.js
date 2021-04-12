5da896581abaead5a336ee5ee63e901c65073724
Ca Lu <Ca.Lu@scc.com>
2021-04-12 10:59:40 +0700

    add new func GameClientUtils

--- demo-app-vs-code/src/Server Development/Script Includes/GameClientUtils.script.js
+++ demo-app-vs-code/src/Server Development/Script Includes/GameClientUtils.script.js
@@ -1,5 +1,7 @@
 var GameClientUtils = Class.create();
 GameClientUtils.prototype = Object.extendsObject(global.AbstractAjaxProcessor, {
-
+    hello:function(){
+        return "Dev01 add new function!";
+    },
     type: 'GameClientUtils'
 });
